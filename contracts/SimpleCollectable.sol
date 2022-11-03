50c935a1dd2502ac19496ec6662cb5bd00219535
Tenorepy <mentorfps@gmail.com>
2022-11-03 23:50:23 +0400

    Nearly Finished

---
+++ contracts/SimpleCollectable.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract SimpleCollectible is ERC721URIStorage {
+    uint256 public tokenCounter;
+
+    constructor() public ERC721("Dogie", "DOG") {
+        tokenCounter = 0;
+    }
+
+    function createCollectable(string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        uint256 newTokenId = tokenCounter;
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        tokenCounter = tokenCounter + 1;
+        return newTokenId;
+    }
+}
